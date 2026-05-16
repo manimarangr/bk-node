ef202ebf1c39f6d3d047d525c4ea8cdf5dc34f20
manimarangr <maran.php@gmail.com>
2026-05-16 10:59:04 +0530

    Docker file added

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://0.0.0.0:27017/bezkoder_db"
+  //url: "mongodb://0.0.0.0:27017/bezkoder_db"
+  url: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/bezkoder_db"
 };
